326b2e54c51a03a3447523111dcdfd028616d9cb
Jack Madden <jck13mad@gmail.com>
2020-01-31 03:28:32 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,7 +3,7 @@ def my_collect(array)
   name_collection
   
   while i < array.length
-    name_collection.push yield(array[i])
+    name_collection << yield(array[i])
     i += 1
   end
   name_collection
