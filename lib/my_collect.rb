3df2f99a6f55bab3c81b6c03b2c1bb82cf6f849e
Jack Madden <jck13mad@gmail.com>
2020-01-31 03:34:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,12 +1,14 @@
 def my_collect(array)
-  if 
-  i = 0
-  name_collection
+  if array.empty?
+    i = 0
+    name_collection
   
-  while i < array.length
-    name_collection << yield(array[i])
-    i += 1
-  end
-  name_collection
+    while i < array.length
+      name_collection << yield(array[i])
+      i += 1
+    end
+    name_collection
+  else
+    puts "Hey! There's nothing in that array!"
 end
 
